5c39c08459c1d62984a565158f469aede180e2b2
Hassan <m.hassan252002@gmail.com>
2023-11-16 16:24:05 +0500

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import Nav from './Nav';
+import Nav from './components/Nav';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +17,7 @@ function App() {
         <Route path='/profile' element={<h1>Profile Component</h1>}/>
       </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
